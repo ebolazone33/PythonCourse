9609748a9c446aeded9db668517f6d56feb3a78a
ebolazone33 <56939719+ebolazone33@users.noreply.github.com>
2019-10-30 19:06:24 -0400

    Add files via upload
    area & volume in python

---
+++ area.py
@@ -0,0 +1,19 @@
+def areaRectangle():
+    x=3
+    y=4
+    print(x*y)
+
+areaRectangle()
+
+def areaCircle():
+    r=3
+    PI=3.14
+    print(PI*r*r)
+areaCircle()
+
+def volumeSphere():
+    PI=3.14
+    r=6
+    V= 4.0/3.0*PI*r**3
+    print (V)
+volumeSphere()
---
+++ date.py
@@ -0,0 +1,21 @@
+#print("30OCTOBER2019")
+
+print("Happy_Halloween")
+
+List1=["car", "boat", "train"]
+#print List1[-1]
+#print List1[-2]
+
+Lisa=["rules","lisa_is_awesome","rocks"]
+#print Lisa[-2]
+
+Tuple1=("foggy","rainy","damp")
+print Tuple1[0]
+
+x="Hello World"
+print(x)
+
+y="Super"
+#print("Python is (y)")
+
+list=[1,4,3,2,5,7,8,9]
