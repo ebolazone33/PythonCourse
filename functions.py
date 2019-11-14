19f7033d24c8839215454414a934518a9d02a92c
ebolazone33 <56939719+ebolazone33@users.noreply.github.com>
2019-11-13 19:11:45 -0500

    Add files via upload
    class 4

--- functions.py
+++ functions.py
@@ -1,11 +1,91 @@
-def list():
-    x= [1,2,3]
-    x.extend([4,5,6,7])
-    print(x)
+ # def Class3():
+ #     a="Lisa"
+ #     for x in a:
+ #         print (x)
+ #
+ # Class3()
 
-def transport():
-    transport= ["car", "boat", "train"]
-    print transport[0]
+# def Class():
+#     a="Steve"
+#     for x in a:
+#         print (x)
+# Class()
+
+# def interate2():
+#     lista = [1, 2, 3]
+#     listb = []
+#     for number in lista:
+#         listb.append(number+3)
+#     print(listb)
+# interate2()
+
+# def interate3():
+#     listc = [12, 22, 78]
+#     listd = []
+#     for number in listc:
+#         listd.append(number*3)
+#     print(listd)
+# interate3()
+
+# def ifelse1():
+#     list1 = [3, 33, 333, 3333]
+#     list2 = []
+#     list3 = []
+#     for number in list1:
+#         if number >50:
+#             list2.append(number)
+#         else:
+#             list3.append(number)
+#     print (list2)
+#     print (list3)
+# ifelse1()
+
+# def Counter():
+#     Counter = 0
+#     while Counter <10:
+#         Counter +=1
+#     print (Counter)
+#
+# Counter()
+
+# def Counter():
+#     Counter = 3
+#     while Counter <10:
+#         Counter +=2
+#     print (Counter)
+# Counter()
+
+
+class Person:
+    name = "Marty"
+    age = 22
+
+    def __init__(self,name1,age1):
+        self.name = name1
+        self.age = age1
+
+Person()
+print(Person.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
